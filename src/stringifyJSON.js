93234c41fc77f3f5b7f4dab0f8e79046ea7d86a6
Alex <alexanderjeng@gmail.com>
2014-08-15 15:37:03 -0700

    Working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -12,103 +12,82 @@ var stringifyJSON = function(obj) {
 
 
   if(typeof obj === "number"){
-  	console.log('is a number');
   	return String(obj);
   }
   else if (obj === null){
-  	console.log('object is null');
   	return "null";
   }
   else if (typeof obj === "undefined"){
-  	console.log('object is undefined');
   	return "undefined";
   }
   else if(typeof obj === "boolean"){
-  	console.log('object is a bool');
   	return obj ? "true" : "false";
   }
   else if (typeof obj === "string"){
-  	console.log('object is a string');
   	return ('"' + obj + '"');
   }
 
-  if(_.isArray(obj)){
+  if(_.isArray(obj)){ //if it's an array, set the end brackets
  		beg = '[';
  		end = ']';
  	}
 
-  else if(_.isObject(obj)){
+  else if(_.isObject(obj)){ //otherwise set the object brackets
   		beg = '{';
   		end = '}';
 
-  		for(var key in obj){
+  		for(var key in obj){ //create list of keys to find end of object
   			keys.push(key);
   		}
-
   }
 
   if(_.isEmpty(obj)){
   	return (beg + end);
   }
 
-   var length = obj.length;
+   var length = obj.length; //variable is set down here because it will exist at this point
+
   _.each(obj, function(item, index){
-  	   console.log('index =' + index);
-  	   console.log('item =' + item);
-  	   //console.log('item[index] = ' + item[index]);
-  	   console.log('key length' + keys.length);
-  	   console.log('counter = ' + counter);
-  	   if(item === undefined || typeof item === 'function'){
+
+  	   if(item === undefined || typeof item === 'function'){ //used for objects that can be stringified 
   	   	 return false;
   	   }
 
   	  if(index > 0 && index <= length-1){ //works only for arrays
   	  	str += (',');
   	  }
-  	  else if(counter > 0 && counter < keys.length){
-  	  	console.log('keys counter');
+  	  else if(counter > 0 && counter < keys.length){ 
   	  	str += (',');
   	  }  	  
+  	  
   	  counter ++;
   	  
-
   	  if(!_.isArray(obj)){
   	  	str += ('"' + index + '":');
   	  }
 	  if(typeof item === "number"){
-	  	console.log('is a number');
 	  	return str += String(item);
 	  }
 	  else if (item === null){
-	  	console.log('object is null');
 	  	return str += "null";
 	  }
 	  else if (typeof item === "undefined"){
-	  	console.log('object is undefined');
 	  	return str += "undefined";
 	  }
 	  else if(typeof item === "boolean"){
-	  	console.log('object is a bool');
 	  	var itemBool = item ? "true" : "false";
 	  	return str += itemBool;
 	  }
 	  else if (typeof item === "string"){
-	  	console.log('object is a string');
 	  	return str += ('"' + item + '"');
 	  }
 	  else if(Object.prototype.toString.call(item) === '[object Array]'){
 	  	str += stringifyJSON(item);
-	  	console.log('array within each');
 	  }
 	  else if (typeof item === 'object'){
 	  	str += stringifyJSON(item);
 	  }
-
-
-	  
   	});
-  	
-  
 
   return (beg + str + end);
 };
